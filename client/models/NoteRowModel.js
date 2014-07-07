66f214cb55a18a9410ae2134ae0ac16eeaa83a17
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-07 16:51:18 -0700

    Initializes NoteRowModel with collection properly

--- client/models/NoteRowModel.js
+++ client/models/NoteRowModel.js
@@ -1,3 +1,13 @@
-// SongModel.js - Defines a backbone model class for songs.
+// NEEDS A COLLECTION ARGUMENT TO BE INSTANTIATED
 var NoteRowModel = Backbone.Model.extend({
+  // defaults: {
+  //   noteRowCollection: new NoteRowCollection()
+  // },
+  initialize: function() {
+    // this.set('noteRowCollection', new NoteRowCollection([]));
+    var firstNote = this.get('rowCollection').first();
+    // var lastNote = this.get('noteModelCollection').last();
+    this.set('current', firstNote);
+  },
+  
 });
