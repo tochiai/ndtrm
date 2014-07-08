a93655f08c99eeeca6839f86e8f8197a0706bca2
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-08 08:51:51 -0700

    Remove server

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/../client'));
-console.log(__dirname + '../client');
-
-app.listen(process.env.PORT || 3000);
